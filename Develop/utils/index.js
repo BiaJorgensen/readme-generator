fc42dc9fccf570e6f8bda7ea83e0ebb5babceb0a
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-11 17:29:42 -0600

    Added .prompt and .then to inti function; Added code to writeToFile function to generate readme

--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -51,16 +51,25 @@ const questions = [{
     message: 'What is your email?',
   }];
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(data) {
+  fs.writeFile('README.md', generateMarkdown(data), (err) => {
+    if (err) throw err;
+    console.log('README.md has been created');
+  })
+}
 
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
-
-
-inquirer
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
   .prompt(questions)
+    .then((data) => {writeToFile(data)})
+    
+ 
+}
+
+// Function call to initialize app
+init();
+
+
+
